33752b17742c1a6232d3436f0887506880697b45
Richard E <thedrummer_88@hotmail.com>
2022-09-05 16:42:51 +0800

    More initial core settings to models.

--- resume/ric/models.py
+++ resume/ric/models.py
@@ -10,6 +10,9 @@ class Education(models.Model):
     end_date = models.DateField()
     comments = models.TextField(max_length=1000)
 
+    class Meta:
+        ordering = ['-end_date']
+
     def __str__(self):
         return "{} at {}".format(self.major, self.institution)
 
@@ -17,7 +20,10 @@ class Education(models.Model):
 class Subject(models.Model):
     education = models.ForeignKey(Education, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    weight = models.DecimalField(decimal_places=2, max_digits=3, default=1.0)
+    weight = models.DecimalField(decimal_places=3, max_digits=4, default=0)
+
+    class Meta:
+        ordering = ['education', '-weight']
 
     def __str__(self):
         return self.name
@@ -28,6 +34,9 @@ class Experience(models.Model):
     job_title = models.CharField(max_length=25)
     start_date = models.DateField()
     end_date = models.DateField(null=True)
+
+    class Meta:
+        ordering = ['-end_date']
 
     def __str__(self):
         return "{} at {}".format(self.job_title, self.organization)
