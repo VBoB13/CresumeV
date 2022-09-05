6a6a2c7c4438625e9f0ecbbfb494565cb2ecaed1
Richard E <thedrummer_88@hotmail.com>
2022-09-05 15:55:24 +0800

    Initial setup for resume website.

--- resume/ric/admin.py
+++ resume/ric/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Education, Subject, Experience, Location
+
 # Register your models here.
+admin.site.register(Education)
+admin.site.register(Subject)
+admin.site.register(Experience)
+admin.site.register(Location)
--- resume/ric/models.py
+++ resume/ric/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Education(models.Model):
+    major = models.CharField(max_length=50)
+    institution = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    comments = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return "{} at {}".format(self.major, self.institution)
+
+
+class Subject(models.Model):
+    education = models.ForeignKey(Education, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    weight = models.DecimalField(decimal_places=2, max_digits=3, default=1.0)
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    organization = models.CharField(max_length=50)
+    job_title = models.CharField(max_length=25)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+
+    def __str__(self):
+        return "{} at {}".format(self.job_title, self.organization)
+
+
+class Location(models.Model):
+    experience = models.ForeignKey(
+        Experience, null=True, on_delete=models.CASCADE)
+    education = models.ForeignKey(
+        Education, null=True, on_delete=models.CASCADE)
+    country = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+
+    def __str__(self):
+        return "{}, {}".format(self.city, self.country)
